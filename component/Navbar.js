7e69cfaac57f00bdeb86dfa52aba5310b1931727
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-09 11:16:45 +0530

    initiate the project

---
+++ component/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+
+        <nav className='min-w-4xl mx-20 my-2 py-4 px-5 bg-white'>
+
+            <div className="logo " >
+               <img className='h-10' src="linktree.svg" alt="linktree" />
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar

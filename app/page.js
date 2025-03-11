eb0d1a35be092e77634ae163018d70c6ed843fb8
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-11 11:13:02 +0530

    make homepage layout with grid and add left side grid

--- app/page.js
+++ app/page.js
@@ -4,12 +4,30 @@ export default function Home() {
   return (
     <main>
 
-      <section className="bg-[#254f1a] min-h-[100vh]" >
- 
+      <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2" >
+
+        <div className=" text-[#d2e823] flex flex-col justify-center items-center ml-[5vw] gap-4 ">
+
+          <p className=" font-extrabold text-7xl" >Everything you are. In one, simple link in bio.</p>
+
+          <p className="text-lg font-semibold" >Join 50M+ people using Linktree for their link in bio. One link to help you share everything you create, curate and sell from your Instagram, TikTok, Twitter, YouTube and other social media profiles.</p>
+
+          <div className="buttons w-full " >
+            <input className="bg-[#fff;] rounded-lg px-4 py-3 text-black" type="text" placeholder="linktr.ee/" />
+            <button className="bg-[#e9c0e9] px-5 py-4 font-semibold rounded-full mx-3 text-black" >Claim your Linktree</button>
+          </div>
+
+        </div>
+
+        <div className=" bg-black flex flex-col justify-center items-center mr-[5vw] gap-2.5 ">
+
+          grid 2
+
+        </div>
 
       </section>
       <section className="bg-[#c4fbb7] min-h-[100vh]" >
- 
+
 
       </section>
 

a5660c12bfa1244d35e3a07b8c5773813ab1c65e
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-14 12:02:06 +0530

    create generate page form and make it step

--- app/generate/page.js
+++ app/generate/page.js
@@ -2,18 +2,31 @@ import React from 'react'
 
 const Generate = () => {
     return (
-        <div className=' grid grid-cols-2 bg-[#254f1a] min-h-screen ' >
-            <div className="col1 flex flex-col justify-center items-start ml-[5vw] gap-10 ">
-                <h1 className=' text-6xl font-extrabold ' >Welcome back!</h1>
-                <input className='bg-green-50 p-3'  type="text" placeholder=' Email or Usename' />
-                <button> Continue</button>
-                <p> OR </p>
+        // <div className=' grid grid-cols-2 bg-[#254f1a] min-h-screen ' >
+        <div className=' grid grid-cols-2 bg-[#225abf]  min-h-screen ' >
+            <div className="col1 flex flex-col justify-center items-center ml-[5vw] gap-10 ">
+
+                <h1 className=' text-5xl font-extrabold my-5 ' >Create your Bittree</h1>
+
+                <div className='flex flex-col gap-3' >
+
+                    <h2 className='font-semibold my-2 '> Step 1: Claim your Handle </h2>
+                    <input className='bg-green-50 p-3 rounded-full px-5 focus:outline-pink-600 ' type="text" placeholder=' Choose a handle' />
+
+                    <h2 className='font-semibold my-2 '> Step 2: Add Links </h2>
+                    <input className='bg-green-50 p-3 rounded-full px-5 focus:outline-pink-600 ' type="text" placeholder=' Enter link text' />
+                    <input className='bg-green-50 p-3 rounded-full px-5 focus:outline-pink-600 ' type="text" placeholder=' Enter link ' />
+
+                    <h2 className='font-semibold my-2 '> Step 3: Add Picture and Finalize </h2>
+
+                </div>
 
             </div>
-            <div className="col2 w-full min-h-screen bg-blue-50">
-                <div className='object-cover' >
-                    <img className=' min-h-screen w-full object-cover' src="/generate.png" alt="generate" />
-                </div>
+
+            <div className="col2 w-full h-screen flex justify-center items-center" >
+
+                <img className=' h-screen w-full object-contain' src="/generate.png" alt="generate" />
+
             </div>
         </div>
     )

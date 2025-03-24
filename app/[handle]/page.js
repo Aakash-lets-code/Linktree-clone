f102067fc0aa75535c49f8ff72a71a49890e632e
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-24 11:40:36 +0530

    fetch and display user profile and links based on handle

--- app/[handle]/page.js
+++ app/[handle]/page.js
@@ -1,11 +1,39 @@
+import Link from "next/link"
+import clientPromise from "@/lib/mongodb"
+
 export default async function Page({ params }) {
     const { handle } = await params
-    const item = [ "copy the object from database"]
-        
-    return <div className="bg-purple-500 min-h-screen flex justify-center items-center" >
-       <div className="photo flex flex-col items-center" >
-              <img src={item.image} alt="profile image" />
-              <span className="text-3xl font-bold" >@{item.handle}</span>
-       </div>
+    const client = await clientPromise;
+    const db = client.db("bittree")
+    const collection = db.collection("links")
+
+    // if the handle is already claimed , you cannot create bittree
+
+    const item = await collection.findOne({ handle: handle })
+
+    const item2 = ["copy the object from database"]
+
+    return <div className="bg-purple-500 min-h-screen flex justify-center items-start my-10" >
+
+        {item && <div className="photo flex flex-col items-center gap-4" >
+
+            <img src={item.image} alt="profile image" />
+
+            <span className="text-3xl font-bold" >@{item.handle}</span>
+            <span className="desc w-80 text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta sequi optio eum!
+            </span>
+
+            <div className="link">
+                {item.links.map((item, index) => {
+                    return <Link key={index} href={item.link} > <div className="py-4 px-2 rounded-md my-3 bg-slate-100 min-w-96 flex justify-center items-center">
+                        {item.linktext}
+                        {item.link}
+                    </div> </Link>
+                })}
+            </div>
+
+        </div>}
+
     </div>
 }

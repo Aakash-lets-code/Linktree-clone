6d2792234a6f5c83ad44b1caef654a555e90df22
Aakash lets code <akashshrivastav071@gmail.com>
2025-04-11 10:59:02 +0530

    refactor layout and enhance image hover effects in Learn

--- app/learn/page.js
+++ app/learn/page.js
@@ -32,17 +32,19 @@ const Learn = () => {
                 </div>
             </div>
 
-            <div className='min-h-screen w-full bg-amber-900'>
+            <div className='min-h-screen w-full'>
 
-                <div className=' grid grid-cols-3 w-[90vw] mx-auto ml-[61px] gap-x-5 bg-purple-500 min-h-screen py-32'>
+                <div className=' grid grid-cols-3 w-[90vw] mx-auto ml-[61px] gap-x-5 min-h-screen py-32'>
 
-                    <div className='bg-purple-400 overflow-hidden'>
-                        <img src="learn-scroll.jpg" alt="image" className='h-[450px] w-full object-cover' />
-                        <div className="text">
-                            <div className=' text-black-400' >
-                            April 3, 2025 | Branding | 5 min read
+                    <div className='group w-full overflow-hidden'>
+                        <div className='max-h-[450px] wfull overflow-hidden ' >
+                        <img src="learn-scroll.jpg" alt="image" className='object-cover origin-center duration-300 transition-all group-hover:scale-105 ' />
+                        </div>
+                        <div className="text space-y-3 mt-7 group-hover:scale-101 origin-center duration-300 transition-all ">
+                            <div className=' text-black-400 flex flex-row gap-x-4 ' >
+                            <span>April 3, 2025 </span>| <span>Branding</span> | <span>5 min read</span>
                             </div>
-                            <h3 className='text-2xl font-extrabold text-black mb-1.5'>
+                            <h3 className='text-3xl font-extrabold text-black tracking-tighter mb-4'>
                             Lissette Calveiro's Tips for Building a Personal Brand
                             </h3>
                             <div className='text-lg text-black'>
@@ -51,13 +53,15 @@ const Learn = () => {
                         </div>
                     </div>
 
-                    <div className='bg-purple-400 overflow-hidden'>
-                        <img src="learn-scroll.jpg" alt="image" className='h-[450px] w-full object-cover' />
-                        <div className="text">
-                            <div className=' text-black-400' >
-                            April 3, 2025 | Branding | 5 min read
+                    <div className='group w-full overflow-hidden'>
+                        <div className='max-h-[450px] wfull overflow-hidden ' >
+                        <img src="learn-scroll.jpg" alt="image" className='object-cover origin-center duration-300 transition-all group-hover:scale-105 ' />
+                        </div>
+                        <div className="text space-y-3 mt-7 group-hover:scale-101 origin-center duration-300 transition-all ">
+                            <div className=' text-black-400 flex flex-row gap-x-4 ' >
+                            <span>April 3, 2025 </span>| <span>Branding</span> | <span>5 min read</span>
                             </div>
-                            <h3 className='text-2xl font-extrabold text-black mb-1.5'>
+                            <h3 className='text-3xl font-extrabold text-black tracking-tighter mb-4'>
                             Lissette Calveiro's Tips for Building a Personal Brand
                             </h3>
                             <div className='text-lg text-black'>
@@ -66,13 +70,15 @@ const Learn = () => {
                         </div>
                     </div>
 
-                    <div className='bg-purple-400 overflow-hidden'>
-                        <img src="learn-scroll.jpg" alt="image" className='h-[450px] w-full object-cover' />
-                        <div className="text">
-                            <div className=' text-black-400' >
-                            April 3, 2025 | Branding | 5 min read
+                    <div className='group w-full overflow-hidden'>
+                        <div className='max-h-[450px] wfull overflow-hidden ' >
+                        <img src="learn-scroll.jpg" alt="image" className='object-cover origin-center duration-300 transition-all group-hover:scale-105 ' />
+                        </div>
+                        <div className="text space-y-3 mt-7 group-hover:scale-101 origin-center duration-300 transition-all ">
+                            <div className=' text-black-400 flex flex-row gap-x-4 ' >
+                            <span>April 3, 2025 </span>| <span>Branding</span> | <span>5 min read</span>
                             </div>
-                            <h3 className='text-2xl font-extrabold text-black mb-1.5'>
+                            <h3 className='text-3xl font-extrabold text-black tracking-tighter mb-4'>
                             Lissette Calveiro's Tips for Building a Personal Brand
                             </h3>
                             <div className='text-lg text-black'>

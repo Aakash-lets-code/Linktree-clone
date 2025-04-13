00b0a847db0de8cc185bbe8d357f085d0d1cba4f
Aakash lets code <akashshrivastav071@gmail.com>
2025-04-13 09:03:17 +0530

    update widht of learn dropdown in navbar

--- component/Navbar.js
+++ component/Navbar.js
@@ -47,16 +47,16 @@ const Navbar = () => {
 
                     <li onMouseEnter={handleLearnEnter} onMouseLeave={handleLearnLeave} className=' hover:bg-[#eff0ec] text-[#74776d] px-4 py-3 rounded-lg relative' > Learn
 
-                        {isLearnHovered && <> <div className='absolute left-0 top-full w-48 z-10 origin-top duration-500 bg-transparent' >
+                        {isLearnHovered && <> <div className='absolute left-0 top-full w-80 z-10 origin-top duration-500 bg-transparent' >
                             <div className='mt-7 px-2 py-1 bg-white rounded-lg shadow-lg '>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >The 2024 Creator Report</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >All Article</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >Creator</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >Trends</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >Best Practices</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >Company</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >Product News</div>
-                                <div className=' p-4 py-3 rounded-lg hover:bg-[#eff0ec] my-1' >Help</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >The 2024 Creator Report</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >All Article</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >Creator</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >Trends</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >Best Practices</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >Company</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >Product News</div>
+                                <div className=' p-4 py-2 rounded-lg hover:bg-[#eff0ec] my-1 ' >Help</div>
                             </div>
                         </div></>}
 

aae37c9f205b8fa77914de4ed9aa0a7757ab8c39
Aakash lets code <akashshrivastav071@gmail.com>
2025-04-19 15:23:00 +0530

    add detailed features for pricing plans and include check icon SVG

--- app/pricing/page.js
+++ app/pricing/page.js
@@ -31,29 +31,181 @@ const Pricing = () => {
                         </div>
 
                         <div className='plans min-h-screen w-[96%] bg-black pt-8 grid grid-cols-4 gap-4 mb-36 ' >
-                            <div className='h-full w-full bg-[#fff] rounded-4xl min-h-[150vh] '> 
+                            <div className='h-full w-full bg-[#fff] rounded-4xl min-h-[150vh] '>
                                 <div className='type pt-12 pb-6 px-6 bg-[#bbbeac] text-3xl font-semibold rounded-t-4xl'>
                                     <h3>Free</h3>
                                 </div>
-                                <div className="text"></div>
+                                <div className="text my-5 px-6 text-[#1e2330] font-semibold text-[20px] gap-y-3 flex flex-col justify-center">
+
+                                    <p>Boost your digital presence <br /> and monetize your socials</p>
+                                    <p className='text-sm' >Key features: <br /> </p>
+                                    <div className="mt-6 mb-10">
+
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Unlimited link </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Multi-factor authentication </strong> for an additional layer of security
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Drive your offline traffic online with  <strong> QR codes </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Grow your <strong> cross-channel following </strong> and connect all of your social profiles with one easy link
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Sell your merchandise and affiliate products with our <strong> Shop </strong> tab
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
-                            <div className='h-full w-full bg-[#fff] rounded-4xl min-h-[150vh] '> 
+                            <div className='h-full w-full bg-[#fff] rounded-4xl min-h-[150vh] '>
                                 <div className='type pt-12 pb-6 px-6 bg-[#bbbeac] text-3xl font-semibold rounded-t-4xl'>
                                     <h3>Starter</h3>
                                 </div>
-                                <div className="text"></div>
+                                <div className="text my-5 px-6 text-[#1e2330] font-semibold text-[20px] gap-y-3 flex flex-col justify-center">
+
+                                    <p>Boost your digital presence <br /> and monetize your socials</p>
+                                    <p className='text-sm' >Key features: <br /> </p>
+                                    <div className="mt-6 mb-10">
+
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Unlimited link </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Multi-factor authentication </strong> for an additional layer of security
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Drive your offline traffic online with  <strong> QR codes </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Grow your <strong> cross-channel following </strong> and connect all of your social profiles with one easy link
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Sell your merchandise and affiliate products with our <strong> Shop </strong> tab
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
-                            <div className='h-full w-full bg-[#fff] text-white rounded-4xl min-h-[150vh] '> 
+                            <div className='h-full w-full bg-[#fff] text-white rounded-4xl min-h-[150vh] '>
                                 <div className='type pt-12 pb-6 px-6 bg-[#502274] text-3xl font-semibold rounded-t-4xl'>
                                     <h3>Pro</h3>
                                 </div>
-                                <div className="text"></div>
+                               <div className="text my-5 px-6 text-[#1e2330] font-semibold text-[20px] gap-y-3 flex flex-col justify-center">
+
+                                    <p>Boost your digital presence <br /> and monetize your socials</p>
+                                    <p className='text-sm' >Key features: <br /> </p>
+                                    <div className="mt-6 mb-10">
+
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Unlimited link </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Multi-factor authentication </strong> for an additional layer of security
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Drive your offline traffic online with  <strong> QR codes </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Grow your <strong> cross-channel following </strong> and connect all of your social profiles with one easy link
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Sell your merchandise and affiliate products with our <strong> Shop </strong> tab
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
-                            <div className='h-full w-full bg-[#fff] rounded-4xl min-h-[150vh] '> 
+                            <div className='h-full w-full bg-[#fff] rounded-4xl min-h-[150vh] '>
                                 <div className='type pt-12 pb-6 px-6 bg-[#bbbeac] text-3xl font-semibold rounded-t-4xl'>
                                     <h3>Premium</h3>
                                 </div>
-                                <div className="text"></div>
+                                <div className="text my-5 px-6 text-[#1e2330] font-semibold text-[20px] gap-y-3 flex flex-col justify-center">
+
+                                    <p>Boost your digital presence <br /> and monetize your socials</p>
+                                    <p className='text-sm' >Key features: <br /> </p>
+                                    <div className="mt-6 mb-10">
+
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Unlimited link </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                <strong> Multi-factor authentication </strong> for an additional layer of security
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Drive your offline traffic online with  <strong> QR codes </strong>
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Grow your <strong> cross-channel following </strong> and connect all of your social profiles with one easy link
+                                            </p>
+                                        </div>
+                                        <div className='py-4 pr-4 flex items-start border-y border-[1px] border-[#d1c3cd] gap-2 text-sm' >
+                                            <img src="check.svg" alt="check" />
+                                            <p>
+                                                Sell your merchandise and affiliate products with our <strong> Shop </strong> tab
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
 

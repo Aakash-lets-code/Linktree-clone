7c5c498d1b479431c153d63f87e73dcb2600b23e
Aakash lets code <akashshrivastav071@gmail.com>
2025-04-27 10:23:56 +0530

    make marketplace component add text and image

---
+++ app/marketplace/page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Markerplace = () => {
+    return (
+        <>
+
+            <section className=' gird grid-rows-2 bg-[#780016]' >
+
+                <div>
+                    <h2 className='text text-8xl tracking-tight font-bold text-[#e9c0e9] letter-spacing-[-0.02em]' >Connect more of you</h2>
+
+                    <p className=' font-semibold text-4xl' >Bring the best experiences across the internet to one place: your Linktree</p>
+
+                </div>
+
+                <div className="image">
+                    <img src="marketplace.webp" alt="image" />
+                </div>
+
+            </section>
+
+        </>
+    )
+}
+
+export default Markerplace

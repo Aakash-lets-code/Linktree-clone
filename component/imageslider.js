8316ce6170781e68766cafc5fa4b733f3f8f0d9a
Aakash lets code <akashshrivastav071@gmail.com>
2025-06-12 11:29:14 +0530

    refactor ImageGallery and globals.css: adjust animation duration and hover effect styling

--- component/imageslider.js
+++ component/imageslider.js
@@ -49,12 +49,22 @@ const ImageGallery = () => {
   return (
     <div className="overflow-hidden w-full py-5">
       <div className="relative flex space-x-4 animate-slide group hover:pause-animation">
+
         {images.map((image, index) => (
           <div key={index} className="relative overflow-hidden" onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)} >
-            <div className={`relative w-full h-full transition-all duration-300 transform ${hoveredIndex === index ? "rotate-y-180 bg-blue-500" : "rotate-y-0 transition-transform"}`} >
+
+            <div className={`relative w-full h-full transition-all duration-300 transform ${hoveredIndex === index ? "rotate-y-180" : "rotate-y-0 transition-transform"}`} >
+
               <img src={image.src} alt={image.alt} className={`${image.width} ${image.height} ${image.rounded} w-full h-full object-cover transition-all duration-300`} />
-              {hoveredIndex === index && (<div className="absolute inset-0 flex items-center justify-center opacity-100 transition-all duration-300">  <span className="text-center text-xl text-invert">{image.text}</span> </div>)}
+
+              {hoveredIndex === index && (
+                <div className={`absolute inset-0 flex items-center justify-center opacity-100 transition-all duration-300 bg-blue-500 ${image.rounded}`}>
+                  <span className="text-center text-xl text-white">{image.text}</span>
+                </div>
+              )}
+
             </div>
+
           </div>
         ))}
       </div>

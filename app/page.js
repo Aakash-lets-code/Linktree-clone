5d0d30c1b5f942a5cd74029186e9e105a6253e89
Aakash lets code <akashshrivastav071@gmail.com>
2025-07-12 08:47:35 +0530

    update notification banner text styling for improved visibility

--- app/page.js
+++ app/page.js
@@ -55,8 +55,8 @@ export default function Home() {
       {visible && <section className="" >
 
         <div className="fixed top-0 left-0 w-full z-auto bg-[#d2e823] text-[#325a1a] md:text-base py-2.5 flex justify-center items-center shadow-md">
-          <p className="text-[15px] font-semibold">
-            Time to convert clicks into customers. Linktree's new growth tools are here!
+          <p className="text-[16px] font-semibold tracking-wider">
+            Time to convert clicks into customers. <u>Linktree's new growth tools are here!</u>
           </p>
           <button className="text-[#375f1a] text-2xl leading-none focus:outline-none absolute right-4 text-center" onClick={() => setVisible(false)}>
             &times;

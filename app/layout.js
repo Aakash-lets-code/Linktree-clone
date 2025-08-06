745370ffc72f5a29e3f146d0d273ffdceaf21cfd
Aakash lets code <akashshrivastav071@gmail.com>
2025-08-06 11:28:39 +0530

    Make layout body relative

--- app/layout.js
+++ app/layout.js
@@ -46,7 +46,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${roboto.variable} ${notoSans.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${roboto.variable} ${notoSans.variable} antialiased relative`}
       >
         <Navbar />
         {children}
